7224aed51b6aa5a19267d2c3bdd38a76f427eb00
AmiRhm08 <amirali.rahmani08@icloud.com>
2026-01-02 17:38:02 +0330

    update

--- main.py
+++ main.py
@@ -33,8 +33,11 @@ SEND_INTERVAL = 3600  # هر ۱ ساعت
 # ================== ویس‌های بوس ==================
 # file_idها رو اینجا بذار
 KISS_VOICE_IDS = [
-    # "AwACAgQAAxkBAA...",
-    # "AwACAgQAAxkBAA...",
+    "AwACAgQAAxkBAAIHomlXo-sRouDBpOTOnhSqmGzm4O5ZAAJiHQAC8sepUq6tTyaCrU-UOAQ",
+    "AwACAgQAAxkBAAIHoWlXo-sgPTbIwYzlZpDENnVu5aPgAAJsHAACSEaBUtd0VP95xXJwOAQ",
+    "AwACAgQAAxkBAAIHo2lXo-sdpuOC5w6I9Arw6DSd2S70AAJjHQAC8sepUqvrlfUXoRxgOAQ",
+    "AwACAgQAAxkBAAIHpGlXo-uoLJD3gCI4JqD9dYrP8-ozAAJkHQAC8sepUlliwAEbMfd0OAQ",
+    "AwACAgQAAxkBAAIHpWlXo-uqxH-jJQbSyMncAAEvFSXPPQACZR0AAvLHqVLe4eMhtHi6LDgE"
 ]
 KISS_VOICE_MEMORY = 3  # چند تای آخر تکرار نشه
 
